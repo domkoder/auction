17fff50043a8afbf6a496ad5a575d5d80c84d2e7
Nandom Alfred <nandommamdam@gmail.com>
2024-05-15 01:21:33 +0100

    created Auction state

--- programs/auction/src/states.rs
+++ programs/auction/src/states.rs
@@ -1,1 +1,32 @@
 use std::mem::size_of;
+use anchor_lang::{
+    prelude::*,
+    solana_program::{
+        clock::UnixTimestamp,
+    },
+};
+
+#[account]
+pub struct Auction {
+    pub auctioneer: Pubkey,
+    pub starting_price: u64,
+    pub leading_bid: u64,
+    pub leading_bidder: Pubkey,
+    pub end_time: i64,
+    pub is_open: bool,
+    pub bump: u8,
+    pub data: String,
+}
+
+impl Auction {
+    pub fn size() -> usize {
+        size_of::<Pubkey>()
+            + size_of::<u64>()
+            + size_of::<u64>()
+            + size_of::<Pubkey>()
+            + size_of::<UnixTimestamp>()
+            + size_of::<bool>()
+            + size_of::<u8>()
+            + size_of::<String>()
+    }
+}
