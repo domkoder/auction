c0b31fcd14f80afb720c996d78373bba4242d3da
Nandom Alfred <nandommamdam@gmail.com>
2024-05-15 10:48:17 +0100

    added some states

--- programs/auction/src/states.rs
+++ programs/auction/src/states.rs
@@ -6,6 +6,11 @@ use anchor_lang::{
     },
 };
 
+#[constant]
+pub const AUCTION_SEED: &[u8] = b"auction";
+#[constant]
+pub const BID_SEED: &[u8] = b"bid";
+
 #[account]
 pub struct Auction {
     pub auctioneer: Pubkey,
@@ -14,19 +19,35 @@ pub struct Auction {
     pub leading_bidder: Pubkey,
     pub end_time: i64,
     pub is_open: bool,
+    // pub name: String,
+    // pub data: String,
     pub bump: u8,
-    pub data: String,
 }
 
 impl Auction {
     pub fn size() -> usize {
         size_of::<Pubkey>()
-            + size_of::<u64>()
-            + size_of::<u64>()
-            + size_of::<Pubkey>()
-            + size_of::<UnixTimestamp>()
-            + size_of::<bool>()
-            + size_of::<u8>()
-            + size_of::<String>()
+        + size_of::<u64>()
+        + size_of::<u64>()
+        + size_of::<Pubkey>()
+        + size_of::<UnixTimestamp>()
+        + size_of::<bool>()
+        + size_of::<u8>()
+        + size_of::<String>()
+    }
+}
+
+#[accounts]
+pub struct Bid {
+    pub bidder: Pubkey,
+    pub amount: u64,
+    pub bump: u8
+}
+
+impl Bid {
+    pub fn size() -> usize {
+        size_of::<Pubkey>()
+        + size_of::<u64>()
+        + size_of::<u8>()
     }
 }
