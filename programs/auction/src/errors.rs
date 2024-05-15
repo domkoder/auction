0417844e574e5078aa43d22412b546eb2612c67f
Nandom Alfred <nandommamdam@gmail.com>
2024-05-15 01:35:58 +0100

    added some costom errors

--- programs/auction/src/errors.rs
+++ programs/auction/src/errors.rs
@@ -1,1 +1,19 @@
 use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum AuctionError {
+    #[msg("Bid offer is lower than current bid")]
+    BidTooLow,
+    #[msg("Already the highest bidder")]
+    AlreadyHighestBidder,
+    #[msg("Wrong account")]
+    WrongAccount,
+    #[msg("Auction is open")]
+    Open,
+    #[msg("Auction is closed")]
+    Closed,
+    #[msg("Invalid operation")]
+    InvalidOperation,
+    #[msg("Winner can not refund")]
+    WinnerRefund,
+}
