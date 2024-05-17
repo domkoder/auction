3990cc3b684c4fc3fca8d65a719ffaeffbc5bf06
Nandom Alfred <nandommamdam@gmail.com>
2024-05-17 16:53:23 +0100

    added some states

--- programs/auction/src/states.rs
+++ programs/auction/src/states.rs
@@ -1,10 +1,5 @@
+use anchor_lang::{prelude::*, solana_program::clock::UnixTimestamp};
 use std::mem::size_of;
-use anchor_lang::{
-    prelude::*,
-    solana_program::{
-        clock::UnixTimestamp,
-    },
-};
 
 #[constant]
 pub const AUCTION_SEED: &[u8] = b"auction";
@@ -13,41 +8,39 @@ pub const BID_SEED: &[u8] = b"bid";
 
 #[account]
 pub struct Auction {
+    // pub id: u32,
     pub auctioneer: Pubkey,
+    pub item: String,
     pub starting_price: u64,
     pub leading_bid: u64,
     pub leading_bidder: Pubkey,
     pub end_time: i64,
     pub is_open: bool,
-    // pub name: String,
-    // pub data: String,
-    pub bump: u8,
+    pub bump: u8
 }
 
 impl Auction {
     pub fn size() -> usize {
         size_of::<Pubkey>()
-        + size_of::<u64>()
-        + size_of::<u64>()
-        + size_of::<Pubkey>()
-        + size_of::<UnixTimestamp>()
-        + size_of::<bool>()
-        + size_of::<u8>()
-        + size_of::<String>()
+            + size_of::<u64>()
+            + size_of::<u64>()
+            + size_of::<Pubkey>()
+            + size_of::<UnixTimestamp>()
+            + size_of::<bool>()
+            + size_of::<String>()
+            + size_of::<u8>()
     }
 }
 
-#[accounts]
+#[account]
 pub struct Bid {
-    pub bidder: Pubkey,
+    pub authority: Pubkey,
     pub amount: u64,
-    pub bump: u8
+    pub bump: u8,
 }
 
 impl Bid {
     pub fn size() -> usize {
-        size_of::<Pubkey>()
-        + size_of::<u64>()
-        + size_of::<u8>()
+        size_of::<Pubkey>() + size_of::<u64>() + size_of::<u8>()
     }
 }
